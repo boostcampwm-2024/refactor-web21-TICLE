2d1f9aba9f6b6ac7dc641f79e2bf01b3f9433f53
seongha <11pi885@gmail.com>
2025-01-16 22:45:12 +0900

    fix: 쿼리 최적화

--- apps/api/src/ticle/dto/getTicleListQueryDto.ts
+++ apps/api/src/ticle/dto/getTicleListQueryDto.ts
@@ -5,14 +5,6 @@ import { SortType } from '../sortType.enum';
 
 export class GetTicleListQueryDto {
   @ApiProperty({
-    example: 1,
-    description: '페이지 번호',
-    default: 1,
-    required: false,
-  })
-  page?: number = 1;
-
-  @ApiProperty({
     example: 10,
     description: '페이지당 항목 수',
     default: 10,
